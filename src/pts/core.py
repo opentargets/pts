580f240e0c00767ac1c8b1146401d98cb3def6de
Javier Ferrer <javier.f.g@um.es>
2025-02-18 13:02:15 +0000

    Use runner name

--- src/pts/core.py
+++ src/pts/core.py
@@ -2,7 +2,7 @@ from otter import Runner
 
 
 def main() -> None:
-    runner = Runner()
+    runner = Runner(name='pts')
     runner.start()
     runner.register_tasks('pts.tasks')
     runner.run()
