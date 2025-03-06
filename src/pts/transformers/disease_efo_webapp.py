95cb824f6f3e254d4acb0670210b9151a441d1d0
Javier Ferrer <jferrer@ebi.ac.uk>
2025-03-06 10:43:43 +0000

    Add disease for webapp task

---
+++ src/pts/transformers/disease_efo_webapp.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import polars as pl
+from loguru import logger
+
+
+def disease_efo_webapp(source: Path, destination: Path) -> None:
+    # load the ontology
+    logger.debug('loading efo')
+    initial = pl.read_parquet(source)
+
+    logger.debug('starting transformation')
+
+    final = initial.select(
+        id=pl.col('id'),
+        parentIds=pl.col('parents'),
+        name=pl.col('name'),
+    )
+
+    # write the result locally
+    final.write_ndjson(destination)
+    logger.info('transformation complete')
