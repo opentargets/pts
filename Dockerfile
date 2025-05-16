36d0bf64faef90fb9ae524e794ad5e04abcab068
Javier Ferrer <jferrer@ebi.ac.uk>
2025-05-16 12:10:07 +0100

    Fix ensembl transformer

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.13.1-slim-bookworm
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends jq
+
 ADD . /app
 
 WORKDIR /app
--- src/pts/transformers/ensembl.py
+++ src/pts/transformers/ensembl.py
@@ -1,65 +1,29 @@
-import json
+import subprocess
+import tempfile
 from pathlib import Path
 
 import polars as pl
 from loguru import logger
 
-
-class FilteredJSONDecoder(json.JSONDecoder):
-    """JSON Decoder that filter keys in the JSON object.
-
-    This decoder calls a hook on each JSON object that filters out keys not in
-    allowed_keys set. It also processes the root_key, so we can get to the data.
-
-    This saves us a lot of memory and time, and serves as a workaround for the
-    bug in polars that causes it to crash when loading large JSON objects:
-
-    https://github.com/pola-rs/polars/issues/17677
-    """
-
-    def __init__(self, root_key='genes', allowed_keys=None, *args, **kwargs):
-        self.root_key = root_key
-        self.allowed_keys = allowed_keys or {
-            'id',
-            'biotype',
-            'description',
-            'end',
-            'start',
-            'strand',
-            'seq_region_name',
-            'name',
-            'approvedSymbol',
-            'transcripts',
-            'SignalP',
-            'Uniprot/SPTREMBL',
-            'Uniprot/SWISSPROT',
-        }
-        super().__init__(*args, **kwargs, object_hook=self.filter_keys)
-
-    def filter_keys(self, obj: dict) -> dict:
-        if self.root_key in obj:
-            return {self.root_key: obj[self.root_key]}
-        return {k: v for k, v in obj.items() if k in self.allowed_keys}
+from pts.schemas.ensembl import schema_ndjson
 
 
 def ensembl(source: Path, destination: Path) -> None:
-    data = json.loads(source.read_bytes(), cls=FilteredJSONDecoder)
+    jq_query = '.genes[] | { id: .id, biotype: .biotype, description: .description, end: .end, start: .start, strand: .strand, chromosome: .seq_region_name, approvedSymbol: .name, transcripts: [.transcripts[] | { id: .id, biotype: .biotype, description: .description, end: .end, start: .start, strand: .strand, chromosome: .seq_region_name, SignalP: .SignalP, "Uniprot/uniprot_trembl": ."Uniprot/SPTREMBL", "uniprot_swissprot": ."Uniprot/SWISSPROT" }]}'  # noqa: E501
+    tempfile_path = Path(f'{tempfile.gettempdir()}/ensembl.jsonl')
 
-    df = pl.from_dicts(data['genes']).select([
-        'id',
-        'biotype',
-        'description',
-        'end',
-        'start',
-        'strand',
-        pl.col('seq_region_name').alias('chromosome'),
-        pl.col('name').alias('approvedSymbol'),
-        'transcripts',
-        'SignalP',
-        pl.col('Uniprot/SPTREMBL').alias('uniprot_trembl'),
-        pl.col('Uniprot/SWISSPROT').alias('uniprot_swissprot'),
-    ])
+    logger.info('transforming ensembl data into a ndjson')
+
+    # we will have to do it like this for now, until polars fixes https://github.com/pola-rs/polars/issues/17677
+    jq = subprocess.run(
+        ['jq', '-c', jq_query, str(source)],
+        capture_output=True,
+        text=True,
+    )
+
+    tempfile_path.write_text(jq.stdout)
+    logger.info('transforming ndjson into parquet')
 
     # write the result locally
-    df.write_parquet(destination, compression='gzip')
+    pl.read_ndjson(tempfile_path, schema=schema_ndjson).write_parquet(destination)
     logger.info('transformation complete')
