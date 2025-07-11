06884d07e92882ce5fb1be6d17a4dc1cfaa2f7f4
Javier Ferrer <jferrer@ebi.ac.uk>
2025-07-11 16:29:05 +0100

    Experimental Pyspark support

---
+++ src/pts/pyspark/common/session.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import loguru
+from pyspark.conf import SparkConf
+from pyspark.sql import SparkSession
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame
+    from pyspark.sql.types import StructType
+
+
+class Session:
+    """This class provides a Spark session and logger."""
+
+    def __init__(
+        self,
+        app_name: str = 'pts',
+        spark_uri: str = 'local[*]',
+        properties: dict[str, str] | None = None,
+    ) -> None:
+        """Initializes a Spark Session."""
+        self.spark: SparkSession = (
+            SparkSession.Builder()
+            .config(conf=self._create_config(properties))
+            .master(spark_uri)
+            .appName(app_name)
+            .getOrCreate()
+        )
+
+        self.logger = loguru.logger
+
+    def _create_config(self, properties: dict[str, str] | None = None) -> SparkConf:
+        if properties is None:
+            properties = {}
+
+        default_properties = {
+            'spark.driver.maxResultSize': '0',
+            'spark.debug.maxToStringFields': '2000',
+            'spark.sql.broadcastTimeout': '3000',
+            # google cloud storage connector
+            'spark.jars.packages': 'com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.21',
+            'spark.hadoop.fs.gs.impl': 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem',
+            'spark.hadoop.fs.AbstractFileSystem.gs.impl': 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS',
+            'spark.sql.adaptive.enabled': 'true',
+            'spark.sql.adaptive.coalescePartitions.enabled': 'true',
+            'spark.serializer': 'org.apache.spark.serializer.KryoSerializer',
+            'spark.hadoop.fs.gs.block.size': '134217728',
+            'spark.hadoop.fs.gs.inputstream.buffer.size': '8388608',
+            'spark.hadoop.fs.gs.outputstream.buffer.size': '8388608',
+            'spark.hadoop.fs.gs.outputstream.sync.min.interval.ms': '2000',
+            'spark.network.timeout': '10s',
+            'spark.network.timeoutInterval': '10s',
+            'spark.executor.heartbeatInterval': '6s',
+            'spark.hadoop.fs.gs.status.parallel.enable': 'true',
+            'spark.hadoop.fs.gs.glob.algorithm': 'CONCURRENT',
+            'spark.hadoop.fs.gs.copy.with.rewrite.enable': 'true',
+            'spark.hadoop.fs.gs.metadata.cache.enable': 'false',
+            'spark.hadoop.fs.gs.auth.type': 'APPLICATION_DEFAULT',
+        }
+
+        properties = {**default_properties, **properties}
+
+        return SparkConf().setAll(list(properties.items()))
+
+    def load_data(
+        self,
+        path: str | list[str],
+        format: str = 'parquet',
+        schema: StructType | str | None = None,
+        **kwargs: bool | float | int | str | None,
+    ) -> DataFrame:
+        """Generic function to read a file or folder into a Spark dataframe.
+
+        The `recursiveFileLookup` flag when set to True will skip all partition
+        columns, but read files from all subdirectories.
+
+        Args:
+            path (str | list[str]): path to the dataset
+            format (str): file format. Defaults to parquet.
+            schema (StructType | str | None): Schema to use when reading the data.
+            **kwargs (bool | float | int | str | None): Additional arguments to
+                pass to spark.read.load. `mergeSchema` is set to True,
+                `recursiveFileLookup` is set to False by default.
+
+        Returns:
+            DataFrame: Dataframe
+        """
+        if schema is None:
+            kwargs['inferSchema'] = kwargs.get('inferSchema', True)
+        kwargs['mergeSchema'] = kwargs.get('mergeSchema', True)
+        kwargs['recursiveFileLookup'] = kwargs.get('recursiveFileLookup', False)
+
+        return self.spark.read.load(path, format=format, schema=schema, **kwargs)
+
+    def stop(self) -> None:
+        """Stops the Spark session."""
+        self.spark.stop()
+        self.logger.info('spark session stopped')
---
+++ src/pts/tasks/pyspark.py
@@ -0,0 +1,79 @@
+from collections.abc import Callable
+from importlib import import_module
+from typing import Self
+
+from loguru import logger
+from otter.task.model import Spec, Task, TaskContext
+from otter.task.task_reporter import report
+
+PYSPARK_PACKAGE = 'pts.pyspark'
+
+type path_or_paths = str | dict[str, str]
+
+type pyspark_entrypoint = Callable[
+    [path_or_paths, path_or_paths, dict[str, str] | None],
+    None,
+]
+
+
+class PysparkSpec(Spec):
+    source: path_or_paths
+    """A string or a dictionary with the source paths."""
+    destination: path_or_paths
+    """A string or a dictionary with the destination paths."""
+    pyspark: str
+    """A string with the entry point of the pyspark program.
+
+        The entry point must be a python file in the `pts.pyspark` package, and
+        it must contain a method with the same name as the file, which takes
+        source, destination and properties as arguments.
+
+        The method should launch a pyspark job to operate on the data read from
+        the input paths, and write the result to the destination paths.
+    """
+    properties: dict[str, str] | None = None
+    """A dictionary with the properties to pass to the pyspark job."""
+
+
+class Pyspark(Task):
+    def __init__(self, spec: PysparkSpec, context: TaskContext) -> None:
+        super().__init__(spec, context)
+        self.spec: PysparkSpec
+        self.pyspark = self.load_pyspark(spec.pyspark)
+
+    @staticmethod
+    def load_pyspark(pyspark_name: str) -> pyspark_entrypoint:
+        try:
+            module = import_module(f'{PYSPARK_PACKAGE}.{pyspark_name}')
+            pyspark: pyspark_entrypoint = getattr(module, pyspark_name)
+            if not callable(pyspark):
+                raise TypeError(f'{pyspark_name} is not a callable')
+            return pyspark
+        except ImportError:
+            raise ModuleNotFoundError(f'{pyspark_name} not found in {PYSPARK_PACKAGE}')
+
+    @report
+    def run(self) -> Self:
+        src, dst = self.spec.source, self.spec.destination
+
+        # build paths based on release_uri or work_path
+        if prefix := self.context.config.release_uri or self.context.config.work_path:
+            if isinstance(src, str):
+                src = f'{prefix}/{src}'
+            elif isinstance(src, dict):
+                src = {k: f'{prefix}/{v}' for k, v in src.items()}
+            if isinstance(dst, str):
+                dst = f'{prefix}/{dst}'
+            elif isinstance(dst, dict):
+                dst = {k: f'{prefix}/{v}' for k, v in dst.items()}
+
+        logger.debug(f'running pyspark job with spec: {self.spec}')
+
+        # run the pyspark job
+        self.pyspark(src, dst, self.spec.properties)
+        logger.info('pyspark job completed successfully')
+
+        # TODO: figure out artifacts
+        self.artifacts = []
+
+        return self
