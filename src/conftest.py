a30354e65552b2c4803639f801dbf108899c24cb
Daniel Suveges <daniel.suveges@protonmail.com>
2025-10-21 12:24:02 +0100

    add java to test environment

--- src/conftest.py
+++ src/conftest.py
@@ -1,4 +1,9 @@
+"""Helper function to enable testing spark functions."""
+
+from typing import Any
+
 import pytest
+from pyspark.sql import SparkSession
 
 from pts.pyspark.common.session import Session
 
@@ -6,6 +11,7 @@ from pts.pyspark.common.session import Session
 @pytest.fixture(scope='session')
 def pts_session():
     """Return the repository Session wrapper (not raw SparkSession).
+
     Scope: session -> start once per test run, stop at the end.
     """
     # Use small resources in CI if desired:
@@ -22,16 +28,22 @@ def pts_session():
         s.stop()
 
 
-@pytest.fixture(scope="session")
-def spark(pts_session):
+@pytest.fixture(scope='session')
+def spark(pts_session: Session) -> SparkSession:
     return pts_session.spark
+
 
 # Inject names into doctest globals
 @pytest.fixture
-def doctest_namespace(spark):
-    """
-    Provide names available inside doctest examples.
+def doctest_namespace(spark: SparkSession) -> dict[str, Any]:
+    """Provide names available inside doctest examples.
 
     The returned dict is merged into the globals for every doctest.
+
+    Args:
+        spark (SparkSession): spark sesion object
+
+    Returns:
+        dict[str, Any]:
     """
-    return {"spark": spark, "pts_session": spark} 
+    return {'spark': spark, 'pts_session': spark}
--- src/pts/pyspark/timeseries_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Modules to calculate timeseries."""
-
-from .association import Association
-from .evidence import Evidence
-
-__all__ = [
-    'Association', 'Evidence'
-]
