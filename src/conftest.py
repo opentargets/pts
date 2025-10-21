aacf7315c406253f7093c1c3533339e569532205
Daniel Suveges <daniel.suveges@protonmail.com>
2025-10-21 15:00:40 +0100

    feat: implementing different timeseries view

--- src/conftest.py
+++ src/conftest.py
@@ -1,11 +1,19 @@
+"""Helper function to enable testing spark functions."""
+
+from typing import TYPE_CHECKING, Any
+
 import pytest
 
 from pts.pyspark.common.session import Session
+
+if TYPE_CHECKING:
+    from pyspark.sql import SparkSession
 
 
 @pytest.fixture(scope='session')
 def pts_session():
     """Return the repository Session wrapper (not raw SparkSession).
+
     Scope: session -> start once per test run, stop at the end.
     """
     # Use small resources in CI if desired:
@@ -22,16 +30,22 @@ def pts_session():
         s.stop()
 
 
-@pytest.fixture(scope="session")
-def spark(pts_session):
+@pytest.fixture(scope='session')
+def spark(pts_session: Session) -> SparkSession:
     return pts_session.spark
+
 
 # Inject names into doctest globals
 @pytest.fixture
-def doctest_namespace(spark):
-    """
-    Provide names available inside doctest examples.
+def doctest_namespace(spark: SparkSession) -> dict[str, Any]:
+    """Provide names available inside doctest examples.
 
     The returned dict is merged into the globals for every doctest.
+
+    Args:
+        spark (SparkSession): spark sesion object
+
+    Returns:
+        dict[str, Any]:
     """
-    return {"spark": spark, "pts_session": spark} 
+    return {'spark': spark, 'pts_session': spark}
