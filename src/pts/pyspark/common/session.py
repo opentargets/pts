f1c5c0b43e21d8533663991a98836f42cffdc376
Javier Ferrer <jferrer@ebi.ac.uk>
2025-11-03 19:07:49 +0000

    get rid of memory settings

--- src/pts/pyspark/common/session.py
+++ src/pts/pyspark/common/session.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 from typing import TYPE_CHECKING
 
-import psutil
 from loguru import logger
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
@@ -18,71 +17,61 @@ class Session:
 
     def __init__(
         self,
-        app_name: str = 'pts',
-        spark_uri: str = 'local[*]',
+        app_name: str = "pts",
+        spark_uri: str = "local[*]",
         properties: dict[str, str] | None = None,
     ) -> None:
         """Initializes a Spark Session."""
-        self.is_dataproc = 'DATAPROC_CLUSTER_NAME' in os.environ
+        self.is_dataproc = "DATAPROC_CLUSTER_NAME" in os.environ
 
         self.spark: SparkSession = (
             SparkSession.Builder()
             .config(conf=self._create_config(properties))
-            .master('yarn' if self.is_dataproc else spark_uri)
+            .master("yarn" if self.is_dataproc else spark_uri)
             .appName(app_name)
             .getOrCreate()
         )
-        self.spark.sparkContext.setLogLevel('WARN')
+        self.spark.sparkContext.setLogLevel("WARN")
 
     def _create_config(self, properties: dict[str, str] | None = None) -> SparkConf:
         if properties is None:
             properties = {}
+        base_properties = {}
 
-        mem_stats = psutil.virtual_memory()
-        available_gb = mem_stats.available // (1024**3)
-        gigs_overhead = max(1, int(available_gb * 0.7))
-        spark_memory = f'{gigs_overhead}g'
+        if not self.is_dataproc:
+            base_properties = {
+                "spark.driver.maxResultSize": "0",
+                "spark.debug.maxToStringFields": "2000",
+                "spark.sql.broadcastTimeout": "3000",
+                # google cloud storage connector
+                "spark.jars.packages": "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.21",
+                "spark.sql.adaptive.enabled": "true",
+                "spark.sql.adaptive.coalescePartitions.enabled": "true",
+                "spark.serializer": "org.apache.spark.serializer.KryoSerializer",
+                "spark.network.timeout": "10s",
+                "spark.network.timeoutInterval": "10s",
+                "spark.executor.heartbeatInterval": "6s",
+                "spark.hadoop.fs.gs.block.size": "134217728",
+                "spark.hadoop.fs.gs.inputstream.buffer.size": "8388608",
+                "spark.hadoop.fs.gs.outputstream.buffer.size": "8388608",
+                "spark.hadoop.fs.gs.outputstream.sync.min.interval.ms": "2000",
+                "spark.hadoop.fs.gs.status.parallel.enable": "true",
+                "spark.hadoop.fs.gs.glob.algorithm": "CONCURRENT",
+                "spark.hadoop.fs.gs.copy.with.rewrite.enable": "true",
+                "spark.hadoop.fs.gs.metadata.cache.enable": "false",
+                "spark.hadoop.fs.gs.auth.type": "APPLICATION_DEFAULT",
+                "spark.hadoop.fs.gs.impl": "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem",
+                "spark.hadoop.fs.AbstractFileSystem.gs.impl": "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS",
+            }
 
-        logger.info(f'using {spark_memory} memory for the spark driver')
+        effective_properties = {**base_properties, **properties}
 
-        dataproc_properties = {}
-        local_properties = {
-            'spark.driver.memory': spark_memory,
-            'spark.driver.maxResultSize': '0',
-            'spark.debug.maxToStringFields': '2000',
-            'spark.sql.broadcastTimeout': '3000',
-            # google cloud storage connector
-            'spark.jars.packages': 'com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.21',
-            'spark.hadoop.fs.gs.impl': 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem',
-            'spark.hadoop.fs.AbstractFileSystem.gs.impl': 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS',
-            'spark.sql.adaptive.enabled': 'true',
-            'spark.sql.adaptive.coalescePartitions.enabled': 'true',
-            'spark.serializer': 'org.apache.spark.serializer.KryoSerializer',
-            'spark.hadoop.fs.gs.block.size': '134217728',
-            'spark.hadoop.fs.gs.inputstream.buffer.size': '8388608',
-            'spark.hadoop.fs.gs.outputstream.buffer.size': '8388608',
-            'spark.hadoop.fs.gs.outputstream.sync.min.interval.ms': '2000',
-            'spark.network.timeout': '10s',
-            'spark.network.timeoutInterval': '10s',
-            'spark.executor.heartbeatInterval': '6s',
-            'spark.hadoop.fs.gs.status.parallel.enable': 'true',
-            'spark.hadoop.fs.gs.glob.algorithm': 'CONCURRENT',
-            'spark.hadoop.fs.gs.copy.with.rewrite.enable': 'true',
-            'spark.hadoop.fs.gs.metadata.cache.enable': 'false',
-            'spark.hadoop.fs.gs.auth.type': 'APPLICATION_DEFAULT',
-        }
-
-        properties = {
-            **(dataproc_properties if self.is_dataproc else local_properties),
-            **properties,
-        }
-
-        return SparkConf().setAll(list(properties.items()))
+        return SparkConf().setAll(list(effective_properties.items()))
 
     def load_data(
         self,
         path: str | list[str],
-        format: str = 'parquet',
+        format: str = "parquet",
         schema: StructType | str | None = None,
         **kwargs: bool | float | int | str | None,
     ) -> DataFrame:
@@ -103,13 +92,13 @@ class Session:
             DataFrame: Dataframe
         """
         if schema is None:
-            kwargs['inferSchema'] = kwargs.get('inferSchema', True)
-        kwargs['mergeSchema'] = kwargs.get('mergeSchema', True)
-        kwargs['recursiveFileLookup'] = kwargs.get('recursiveFileLookup', False)
+            kwargs["inferSchema"] = kwargs.get("inferSchema", True)
+        kwargs["mergeSchema"] = kwargs.get("mergeSchema", True)
+        kwargs["recursiveFileLookup"] = kwargs.get("recursiveFileLookup", False)
 
         return self.spark.read.load(path, format=format, schema=schema, **kwargs)
 
     def stop(self) -> None:
         """Stops the Spark session."""
         self.spark.stop()
-        logger.info('spark session stopped')
+        logger.info("spark session stopped")
