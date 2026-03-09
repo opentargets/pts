24fc5f166149f6aadd0f7d10f0bb7d8c11e0b2df
Daniel Suveges <daniel.suveges@protonmail.com>
2026-03-09 16:09:32 +0000

    fix(evidence_clinical_precedence): adding direction on target column

--- src/pts/pyspark/evidence_utils/validation_lut.py
+++ src/pts/pyspark/evidence_utils/validation_lut.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from itertools import chain
 from typing import Any
 
 from pyspark.sql import Column, DataFrame
@@ -11,29 +10,6 @@ from pyspark.sql import functions as f
 from pyspark.sql import types as t
 
 from pts.pyspark.common.session import Session
-
-MECHANISM_OF_ACTION_MAP = {
-    # Inhibitor list:
-    'RNAI INHIBITOR': 'LoF',
-    'NEGATIVE MODULATOR': 'LoF',
-    'NEGATIVE ALLOSTERIC MODULATOR': 'LoF',
-    'ANTAGONIST': 'LoF',
-    'ANTISENSE INHIBITOR': 'LoF',
-    'BLOCKER': 'LoF',
-    'INHIBITOR': 'LoF',
-    'DEGRADER': 'LoF',
-    'INVERSE AGONIST': 'LoF',
-    'ALLOSTERIC ANTAGONIST': 'LoF',
-    'DISRUPTING AGENT': 'LoF',
-    # Activator list:
-    'PARTIAL AGONIST': 'GoF',
-    'ACTIVATOR': 'GoF',
-    'POSITIVE ALLOSTERIC MODULATOR': 'GoF',
-    'POSITIVE MODULATOR': 'GoF',
-    'AGONIST': 'GoF',
-    'SEQUESTERING AGENT': 'GoF',
-    'STABILISER': 'GoF',
-}
 
 
 @dataclass
@@ -43,7 +19,6 @@ class LookUpTables:
     disease_lut: DataFrame | None = None
     target_lut: DataFrame | None = None
     publication_lut: DataFrame | None = None
-    mechanism_of_action: DataFrame | None = None
 
     def __post_init__(self: LookUpTables) -> None:
         # Process disease:
@@ -57,14 +32,6 @@ class LookUpTables:
         # Process publication:
         if publication_lut_path := self.settings.get('publication_date_lut'):
             self.publication_lut = self._prepare_publication_lut(self.session.load_data(publication_lut_path, 'json'))
-
-        # mechanism of action is optional. Read if available:
-        if mechanism_of_action_lut_path := self.settings.get('mechanism_of_action'):
-            self.mechanism_of_action_lut = self._prepare_moa_lut(
-                self.session.load_data(mechanism_of_action_lut_path, 'parquet')
-            )
-        else:
-            self.mechanism_of_action_lut = None
 
     @staticmethod
     def _prepare_disease_lut(df: DataFrame) -> DataFrame:
@@ -194,54 +161,4 @@ class LookUpTables:
             )
             .when(f.array_contains(flagged, 'oncogene'), f.lit('oncogene'))
             .when(f.array_contains(flagged, 'tsg'), f.lit('tsg'))
-        )
-
-    @staticmethod
-    def _prepare_moa_lut(moa_df: DataFrame) -> DataFrame:
-        """Generate look up table for chembl evidence with unified MoA assessment.
-
-        Args:
-            moa_df (DataFrame): raw mechanism of action dataset.
-
-        Returns:
-            DataFrame: drug/target look up -> moa type.
-        """
-        # Create moa map:
-        moa_mapping_expr = f.create_map([f.lit(x) for x in chain(*MECHANISM_OF_ACTION_MAP.items())])
-
-        return (
-            moa_df
-            # Mapping action types:
-            .withColumn(
-                'moaType',
-                f.coalesce(moa_mapping_expr.getItem(f.col('actionType')), f.lit('noEvaluable')),
-            )
-            # Exploding drugs:
-            .select(
-                f.explode_outer('chemblIds').alias('drugId'),
-                'moaType',
-                'targets',
-            )
-            # Exploding diseases:
-            .select(
-                f.explode_outer('targets').alias('targetId'),
-                'drugId',
-                'moaType',
-            )
-            # Aggregate action types by drug/target pairs:
-            .groupBy('targetId', 'drugId')
-            .agg(
-                f.collect_set('moaType').alias('moaTypes'),
-            )
-            .withColumn(
-                'moaType',
-                f
-                .when(
-                    f.array_contains('moaTypes', 'GoF') & f.array_contains('moaTypes', 'LoF'),
-                    f.lit(None).cast(t.StringType()),
-                )
-                .when(f.array_contains('moaTypes', 'GoF'), f.lit('GoF'))
-                .when(f.array_contains('moaTypes', 'LoF'), f.lit('LoF')),
-            )
-            .select('targetId', 'drugId', 'moaType')
         )
