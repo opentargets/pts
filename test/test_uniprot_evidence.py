743771f6a4b52a09957902b26e69ae5936ba42ae
David Ochoa <ochoa@ebi.ac.uk>
2026-05-11 19:45:10 +0100

    test(uniprot-evidence): multi-disease and edge-case coverage

--- test/test_uniprot_evidence.py
+++ test/test_uniprot_evidence.py
@@ -63,3 +63,51 @@ def test_parse_record_disease_not_polluted_by_copyright_block():
 def test_parse_record_gene_name_from_gn_line():
     rec = _parse_record(_lines(SINGLE_DISEASE_ENTRY))
     assert rec['geneNames'] == ['BRCA1']
+
+
+MULTI_DISEASE_ENTRY = """\
+ID   TP53_HUMAN               Reviewed;         393 AA.
+AC   P04637;
+GN   Name=TP53;
+CC   -!- DISEASE: Li-Fraumeni syndrome 1 (LFS1) [MIM:151623]: Autosomal
+CC       dominant. {ECO:0000269|PubMed:1565144}.
+CC   -!- DISEASE: Esophageal cancer (ESCR) [MIM:133239]: Malignancy.
+CC       {ECO:0000269|PubMed:8632902, ECO:0000269|PubMed:10780666}.
+CC   -!- FUNCTION: Acts as a tumor suppressor.
+"""
+
+
+def test_parse_record_multi_disease_count():
+    rec = _parse_record(_lines(MULTI_DISEASE_ENTRY))
+    assert len(rec['diseases']) == 2
+
+
+def test_parse_record_multi_disease_acronyms():
+    rec = _parse_record(_lines(MULTI_DISEASE_ENTRY))
+    acronyms = [d['acronym'] for d in rec['diseases']]
+    assert acronyms == ['LFS1', 'ESCR']
+
+
+def test_parse_record_disease_block_terminated_by_non_disease_cc():
+    rec = _parse_record(_lines(MULTI_DISEASE_ENTRY))
+    second = rec['diseases'][1]
+    assert second['evidencePmids'] == ['8632902', '10780666']
+    assert 'tumor suppressor' not in second['description']
+
+
+def test_parse_record_no_diseases():
+    entry = """\
+ID   NODIS_HUMAN              Reviewed;         100 AA.
+AC   Q12345;
+GN   Name=NODIS;
+CC   -!- FUNCTION: Some function.
+"""
+    rec = _parse_record(_lines(entry))
+    assert rec['diseases'] == []
+
+
+def test_parse_record_id_and_accession_and_gene():
+    rec = _parse_record(_lines(MULTI_DISEASE_ENTRY))
+    assert rec['id'] == 'TP53_HUMAN'
+    assert rec['accession'] == 'P04637'
+    assert rec['geneNames'] == ['TP53']
