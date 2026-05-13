df478df63ff81ff11325c77d9b97034b14a48e4d
vivienho <56025826+vivienho@users.noreply.github.com>
2026-05-13 21:46:14 +0100

    chore: update ontoma function parameters in line with update in ontoma

--- src/pts/pyspark/literature_ontoma_lut_generation.py
+++ src/pts/pyspark/literature_ontoma_lut_generation.py
@@ -25,9 +25,9 @@ def literature_ontoma_lut_generation(
     drug_index = spark.load_data(path=source['drug_index'])
 
     disease_index_label_lut = OpenTargetsDisease.as_label_lut(disease_index)
-    ot_disease_curation_label_lut = DiseaseCuration.as_label_lut(ot_disease_curation, disease_index, 'ot_curation')
-    eva_clinvar_label_lut = DiseaseCuration.as_label_lut(eva_clinvar, disease_index, 'eva_clinvar')
-    clinvar_xrefs_label_lut = DiseaseCuration.as_label_lut(clinvar_xrefs, disease_index, 'clinvar_xrefs')
+    ot_disease_curation_label_lut = DiseaseCuration.as_label_lut(ot_disease_curation, 'ot_curation', disease_index, True)
+    eva_clinvar_label_lut = DiseaseCuration.as_label_lut(eva_clinvar, 'eva_clinvar', disease_index, True)
+    clinvar_xrefs_label_lut = DiseaseCuration.as_label_lut(clinvar_xrefs, 'clinvar_xrefs', disease_index, True)
     target_index_label_lut = OpenTargetsTarget.as_label_lut(target_index)
     drug_index_label_lut = OpenTargetsDrug.as_label_lut(drug_index)
 
